ae1a38bca5bb05da2b5405f912a18cc94a4b3e5f
CryptoSnowPrince <topdirector2017@gmail.com>
2023-05-29 17:11:38 +0200

    remove emojies

--- messages.py
+++ messages.py
@@ -1,5 +1,5 @@
 message = {
-    "/start": """👋 Hello there!\n\nThis bot returns AI-generated images from any prompt you give, using <a href="https://HelloKittyETH.com">KITTY</a>.\n\n<b>Example:</b> Send a message like <pre>/kitty Condescending Kitty and passionate people who love crypto</pre>\n\nEnjoy! 🟣🍫 #KITTY""",
+    "/start": """👋 Hello there!\n\nThis bot returns AI-generated images from any prompt you give, using <a href="https://HelloKittyETH.com">KITTY</a>.\n\n<b>Example:</b> Send a message like <pre>/kitty Condescending Kitty and passionate people who love crypto</pre>\n\nEnjoy! #KITTY""",
     "/help": "Send a message like <pre>/kitty Condescending Kitty and passionate people who love crypto</pre> to generate a image.",
     "/wait": "The image is being generated. Please wait a few seconds for it..."
 }
